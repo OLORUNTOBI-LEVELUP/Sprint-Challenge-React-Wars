11ef9135d4ac7f0c9ed6c66cd5936ea2ca744813
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-31 09:23:41 +0100

    Displayed the whole characterList I displayed the whole chracter list by creating a p tag for all the properties and iterating through the array blist to display all the character name and properties to the browser

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -6,7 +6,13 @@ const CharacterCard = ({ character }) => {
     return (
       <div >
         <h1>{character.name}</h1>
-        
+        <p>Birth Year: {character.birth_year}</p>
+        <p>Gender: {character.gender}</p>
+        <p>Height: {character.height}cm</p>
+        <p>Weight: {character.mass}kg</p>
+        <p>Eye Color: {character.eye_color}</p>
+        <p>Hair Color: {character.hair_color}</p>
+        <p>Skin Color: {character.skin_color}</p>
       </div>
     )
   }
